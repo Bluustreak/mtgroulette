9593317cf96448ef72557688422101de104350df
Bluustreak <tailsfang@gmail.com>
2024-06-11 17:02:41 +0200

    initialFiles

---
+++ scripts.js
@@ -0,0 +1,16 @@
+function getCommander()
+{
+
+}
+
+function getCards()
+{
+    var cardTypes=["artifact", "creature", "enchantment", "instant", "sorcery"]
+}
+function buttonClick()
+{
+    //q=c%3D{colors}{kw1}{kw2}+t%3Alegendary+t%3Acreature{order}{sorting}
+    let keywords=(document.getElementById("id_keywordsTextField").value).split(" ")
+    alert(keywords)
+
+}
