6602921443c0a78f9fb45a52b39bc696ce6c9171
BluuRazer <tailsfang@gmail.com>
2024-06-13 09:19:04 +0200

    2024-06-12

--- scripts.js
+++ scripts.js
@@ -49,39 +49,57 @@ const ORDER = {
 function buttonClick()
 {
     let inputField=document.getElementById("colors");
-    if (containsOnlyCorrectColors(inputField))
-    {
-        let deck=[];
 
-        let keywords=(document.getElementById("id_keywordsTextField").value).split(" ");
+    let deck = [];
 
-        // default values for a deck, based on grave danger
-        let n_cre = 29;
-        let n_sor = 12;
-        let n_art = 9;
-        let n_ins = 5;
-        let n_enc = 4;
-        let n_pla = 1;
+    let keywords = (document.getElementById("id_keywordsTextField").value).split(" ");
 
-        // adding each card type to the deck
-        deck.push(getCommander(keywords))
-        deck.push(getCards(CREATURE, n_cre, keywords))
-        deck.push(getCards(SORCERY, n_sor, keywords))
-        deck.push(getCards(ARTIFACT, n_art, keywords))
-        deck.push(getCards(INSTANT, n_ins, keywords))
-        deck.push(getCards(ENCHANTMENT, n_enc, keywords))
-        deck.push(getCards(PLANESWALKER, n_pla, keywords))
-        
+    let checkboxW = document.getElementById("CBW");
+    let checkboxB = document.getElementById("CBB");
+    let checkboxG = document.getElementById("CBG");
+    let checkboxU = document.getElementById("CBU");
+    let checkboxR = document.getElementById("CBR");
+
+    let UI_commanderColors = "";
+    if (checkboxW.checked)
+        UI_commanderColors+="W";
+    if (checkboxB.checked)
+        UI_commanderColors+="B";
+    if (checkboxG.checked)
+        UI_commanderColors+="G";
+    if (checkboxU.checked)
+        UI_commanderColors+="U";
+    if (checkboxR.checked)
+        UI_commanderColors+="R";
+    alert(UI_commanderColors);
+
+    // default values for a deck, based on grave danger
+    let n_cre = 29;
+    let n_sor = 12;
+    let n_art = 9;
+    let n_ins = 5;
+    let n_enc = 4;
+    let n_pla = 1;
+
+    UI_order=ASCENDING;
+
+    // adding each card type to the deck
+    deck.push(getCommander(UI_commanderColors, UI_order, keywords))
+    deck.push(getCards(CREATURE, n_cre, UI_commanderColors, UI_order, keywords))
+    //deck.push(getCards(SORCERY, n_sor, keywords))
+    //deck.push(getCards(ARTIFACT, n_art, keywords))
+    //deck.push(getCards(INSTANT, n_ins, keywords))
+    //deck.push(getCards(ENCHANTMENT, n_enc, keywords))
+    //deck.push(getCards(PLANESWALKER, n_pla, keywords))
 
 
 
-        
 
-        
-       
-    }
-    else if(!containsOnlyCorrectColors(inputField))
-        alert("Incorrect colors")
+    
+
+    
+    
+
 
 
 
@@ -104,12 +122,13 @@ function containsOnlyCorrectColors(text)
 
 async function sendRequest(query)
 {
-    const response = await fetch(`https://api.scryfall.com/cards/search?order=set&q=${encodeURIComponent(query)}&unique=prints`);
+    const response = await fetch(`https://api.scryfall.com/cards/search?${(query)}`);
+    alert("trying the query: \n"+`https://api.scryfall.com/cards/search?${(query)}`);
     if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
     }
     reply = response.json();
-    sleep(0.5)
+    sleep(0.5);
     return await reply;
 }
 
@@ -121,10 +140,9 @@ function parseJSON(JSONdata)
     }));
 }
 
-function getCards(cardType, N, UI_order=ASCENDING, UIkeywords)
+function getCards(cardType, N, UI_commanderColors, UI_order, UIkeywords)
 {
-    
-        let query = queryComposer(ASCENDING, cardType, "WUBGR", UIkeywords.split(" "));
+        let query = queryComposer(UI_order, cardType, UI_commanderColors, UIkeywords);
         let JSONdata = sendRequest(query);
         let cards = parseJSON(JSONdata);
         cards = getNcards(cards, N)
@@ -136,50 +154,51 @@ function getCards(cardType, N, UI_order=ASCENDING, UIkeywords)
         let query = `https://scryfall.com/search?as=grid&extras=true&lang=any&order=${UI_order}}&q=type%3A${UI_cardtype}+commander%3A${UI_commanderColors}+%28game%3Apaper%29${KWplaceholder}&unique=cards`;
     }
 }
-function getCommander(UI_order=ASCENDING, UIkeywords)
+function getCommander(UI_commanderColors, UI_order, UIkeywords)
 {
         let KWplaceholder=""
-        for(kw in UIkeywords)
+        for(let kw of UIkeywords)
         {
             KWplaceholder+=`+lore%3A${kw}`
         }
+        let query = `https://api.scryfall.com/cards/search?as=grid&extras=true&lang=any&order=${UI_order}&q=type%3A${"creature"}+type%3A${"legendary"}+c%3D${UI_commanderColors}+%28game%3Apaper%29${KWplaceholder}&unique=cards`;
         let JSONdata = sendRequest(query);
+        alert(query);
         let cards = parseJSON(JSONdata);
-        cards = getNcards(cards, N)
-            // change various things including color
-        let query = `https://scryfall.com/search?as=grid&extras=true&lang=any&order=${UI_order}}&q=type%3A${UI_cardtype}+commander%3A${UI_commanderColors}+%28game%3Apaper%29${KWplaceholder}&unique=cards`;
-    
+        card = getOneRandomCard(cards);
+        deck.push(card);
+        alert(card);
 }
 
-function queryComposer(UI_order=ASCENDING, UI_cardtype, UI_commanderColors, UIkeywords)
+function queryComposer(UI_order, UI_cardtype, UI_commanderColors, UIkeywords)
 {
     // if the user has picked any keywords, those will be added, if not, just a empty string litteral
     let KWplaceholder=""
-    for(kw in UIkeywords)
+    for(let kw of UIkeywords)
     {
-        KWplaceholder+=`+lore%3A${kw}`
+        KWplaceholder+=`+lore%3A${kw}`;
     }
-    let query = `https://scryfall.com/search?as=grid&extras=true&lang=any&order=${UI_order}}&q=type%3A${UI_cardtype}+commander%3A${UI_commanderColors}+%28game%3Apaper%29${KWplaceholder}&unique=cards`;
+        let query = `https://api.scryfall.com/cards/search?as=grid&extras=true&lang=any&order=${UI_order}&q=type%3A${UI_cardtype}+commander%3A${UI_commanderColors}+%28game%3Apaper%29${KWplaceholder}&unique=cards`;
 }
 
 function sleep(milliseconds) {
   const date = Date.now();
-  let currentDate = null;
-  do {
-    currentDate = Date.now();
-  } while (currentDate - date < milliseconds);
+  while(Date.now()-date < milliseconds){}
 }
 
+function getOneRandomCard(cards)
+{
+    let randnum = Math.floor(Math.random()*60);
+    return cards[randnum];
+}
 function getNcards(cards, N)
 {
     // does not repeat cards
-    let counter=0;
     let local_cards = [];
-    while(counter < N)
+    while(local_cards.length() < N)
     {
-        let randnum = Math.floor(Math.random()*60);
-        let card = cards[randnum];
-        if (local_cards.find(card) == undefined)
+        card=getOneRandomCard(cards)
+        if (local_cards.find(card) == undefined) // if the card is not in the deck, add it
         {
             local_cards.push(card);
         }
